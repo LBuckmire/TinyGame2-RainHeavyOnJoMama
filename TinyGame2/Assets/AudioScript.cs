dab62be0172b275c4e52abbbac2f99159e766dc3
HitoriMusuko <Hitori_Musuko@users.noreply.github.com>
2019-10-21 21:04:44 +0100

    Added End Scene
    Added an end scene to the game

---
+++ TinyGame2/Assets/AudioScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    //public AudioClip GameMusic;
+    //public AudioSource MusicSource;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MusicSource = GetComponent<AudioSource>();
+        //MusicSource.clip = GameMusic;
+        //MusicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!MusicSource.isPlaying)
+        {
+            MusicSource.Play();
+        }*/
+    }
+}
